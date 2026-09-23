6672a42f7c0ae8aa02c17a608e3a84b83d2937d8
jitendermehra11-dotcom <jitendermehra11@gmail.com>
2026-09-23 18:14:17 +0530

    Update HomeScreen.kt

--- app/src/main/java/com/example/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/ui/screens/HomeScreen.kt
@@ -85,7 +85,7 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.example.R
+import com.aistudio.loksetu.vxqtmp.R
 import com.example.location.LocationHelper
 import com.example.ui.components.CategorySelectorRow
 import com.example.ui.components.FilterOptionsRow
