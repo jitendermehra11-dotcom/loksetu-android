1ccea70f79cc1ffaee913b4e0ab81a8052cba790
jitendermehra11-dotcom <jitendermehra11@gmail.com>
2026-09-23 17:04:50 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,11 +10,11 @@ plugins {
 }
 
 android {
-  namespace = "com.example"
+  namespace = "com.aistudio.loksetu.vxqtmp"
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
-    applicationId = "com.aistudio.loksetu.vxqtwp"
+  applicationId = "com.aistudio.loksetu.vxqtmp"
     minSdk = 24
     targetSdk = 36
     versionCode = 1
